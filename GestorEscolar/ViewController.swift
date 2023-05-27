5fc192d9646aab393df0d6f7f0ca393ee2a101c4
elaniacs <carensousaesilva@gmail.com>
2023-05-27 09:28:32 -0300

    Initial Commit

---
+++ GestorEscolar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GestorEscolar
+//
+//  Created by Cáren Sousa on 27/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
